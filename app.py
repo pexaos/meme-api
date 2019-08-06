754349d88fee8dac863f419e527b0b0023551315
Emersonsilva <pexaos24@gmail.com>
2019-08-06 14:19:57 -0600

    last version

--- app.py
+++ app.py
@@ -9,35 +9,41 @@ app = Flask(__name__)
 heroku = Heroku(app)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "app.sqlite")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + \
+    os.path.join(basedir, "app.sqlite")
 
 CORS(app)
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
+
 class Meme(db.Model):
-    __tablename__="memes"
+    __tablename__ = "memes"
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(50))
     image = db.Column(db.String(500))
     favorite = db.Column(db.Boolean)
 
     def __init__(self, text, image, favorite):
-        self.text=text
-        self.image=image
-        self.favorite=favorite
+        self.text = text
+        self.image = image
+        self.favorite = favorite
+
 
 class MemeSchema(ma.Schema):
     class Meta:
         fields = ("id", "text", "image", "favorite")
 
+
 meme_schema = MemeSchema()
 memes_schema = MemeSchema(many=True)
 
+
 @app.route("/")
 def greeting():
-    return "<h1> hello</h1>"
+    return "<h1>Meme API</h1>"
+
 
 @app.route("/memes", methods=["GET"])
 def get_memes():
@@ -46,32 +52,49 @@ def get_memes():
     return jsonify(result.data)
 
 
+@app.route("/meme/<id>", methods=["GET"])
+def get_meme(id):
+    meme = Meme.query.get(id)
+    return meme_schema.jsonify(meme)
+
 
 @app.route("/add-meme", methods=["POST"])
 def add_meme():
     text = request.json["text"]
     image = request.json["image"]
     favorite = request.json["favorite"]
-    
-    
+
     new_meme = Meme(text, image, favorite)
-    
+
     db.session.add(new_meme)
     db.session.commit()
-    
+
     return jsonify("MEME POSTED!")
+
 
 @app.route("/meme/<id>", methods=["DELETE"])
 def delete_meme(id):
-    meme_Meme.query.get(id)
+    meme = Meme.query.get(id)
     db.session.delete(meme)
     db.session.commit()
-    
+
     return jsonify("RECORD DELETED")
 
-if  __name__ == "__main__":
-    app.debbug=True
+
+@app.route("/meme/<id>", methods=["PUT"])
+def update_meme(id):
+    meme = Meme.query.get(id)
+
+    new_text = request.json["text"]
+    new_favorite = request.json["favorite"]
+
+    meme.text = new_text
+    meme.favorite = new_favorite
+
+    db.session.commit()
+    return meme_schema.jsonify(meme)
+
+
+if __name__ == "__main__":
+    app.debug = True
     app.run()
-
-
-
